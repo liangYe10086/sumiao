a432d6a41c0a0835440587d010ed3b721194a5ab
root <root@Meituanyun-M60.cloud.mos>
2018-05-16 11:27:34 +0800

    first commit

---
+++ sumiao2.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 15 11:58:35 2018
+
+@author: wangzhaoliang
+"""
+from PIL import Image
+import numpy as np
+import logging
+import os
+import json
+
+logger = logging.getLogger("Sumiao")
+
+class Main():
+    def __init__(self):
+        pass
+         
+    def predict(self,**kwargs):
+        if 'params' in kwargs:
+            params = json.loads(kwargs['params'])
+        else:   
+            logger.info('Must have an input as path param ')
+        param_path = params['image_path']
+        numbers = params['numbers']       
+        one_image_re(param_path,numbers)
+
+def one_image_re(image_path,numbers):
+
+    start = image_path
+    numbers=numbers
+    print('Transferring:   ' + start)
+    sta=start
+    if not os.path.exists('./gen/'):
+        os.mkdir('./gen/')
+        
+    end = './gen/' + 'S_' + sta.split('/')[-1]
+    #函数的核心部分，利用numpy与PIL库进行图像操作。
+    a = np.asarray(Image.open(sta).convert('L')).astype('float')
+    
+    depth = numbers  # (0-100)
+    grad = np.gradient(a)  # 取图像灰度的梯度值
+    grad_x, grad_y = grad  # 分别取横纵图像梯度值
+    grad_x = grad_x * depth / 100.
+    grad_y = grad_y * depth / 100.
+    A = np.sqrt(grad_x ** 2 + grad_y ** 2 + 1.)
+    uni_x = grad_x / A
+    uni_y = grad_y / A
+    uni_z = 1. / A
+    vec_el = np.pi / 2.2  # 光源的俯视角度，弧度值
+    vec_az = np.pi / 4.  # 光源的方位角度，弧度值
+    dx = np.cos(vec_el) * np.cos(vec_az)  # 光源对x 轴的影响
+    dy = np.cos(vec_el) * np.sin(vec_az)  # 光源对y 轴的影响
+    dz = np.sin(vec_el)  # 光源对z 轴的影响
+    b = 255 * (dx * uni_x + dy * uni_y + dz * uni_z)  # 光源归一化
+    b = b.clip(0, 255)
+    im = Image.fromarray(b.astype('uint8'))  # 重构图像
+    im.save(end)
+
+
+
+if __name__=='__main__':
+    
+    ms = Main()
+    dic={ 'numbers' : 5, 'image_path' : './pictures/3.jpg'}
+    ms.predict(params = json.dumps(dic))
